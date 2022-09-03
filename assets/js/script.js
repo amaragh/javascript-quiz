8ddcbcffd182fdba762801337d5836b1f34894a6
amaragh <mercer.alecia@gmail.com>
2022-09-03 14:23:47 -0500

    start code to iterate through questions array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,19 @@
+var headerEl = document.querySelector("header");
+
 var startBtnEl = document.querySelector(".startBtn");
 
 var timerEl = document.querySelector(".timer");
-console.log(timerEl);
 
 var questionContainerEl = document.querySelector(".questions");
 
-var questionEl = document.createElement("h2");
+var questionEl = document.querySelector(".question");
 
-var choiceContainerEl = document.createElement("ul");
-var choice1El = document.createElement("li");
-var choice2El = document.createElement("li");
-var choice3El = document.createElement("li");
-var choice4El = document.createElement("li");
+var choiceContainerEl = document.querySelector(".choices");
 
-questionEl.className = "question";
-choiceContainerEl.className = "choices";
-choice1El.className = "choice";
-choice2El.className = "choice";
-choice3El.className = "choice";
-choice4El.className = "choice";
-
-questionContainerEl.appendChild(questionEl);
-
-questionContainerEl.appendChild(choiceContainerEl);
-choiceContainerEl.appendChild(choice1El);
-choiceContainerEl.appendChild(choice2El);
-choiceContainerEl.appendChild(choice3El);
-choiceContainerEl.appendChild(choice4El);
+var choice1El = document.querySelector(".choice1");
+var choice2El = document.querySelector(".choice2");
+var choice3El = document.querySelector(".choice3");
+var choice4El = document.querySelector(".choice4");
 
 
 var questions = [
@@ -36,7 +23,7 @@ var questions = [
         choice2: "perItem()",
         choice3: "while()",
         choice4: "each()",
-        answer:"for()"
+        answer: "for()"
     },
     {
         question: "Which statement can be used to declare a variable?",
@@ -44,25 +31,67 @@ var questions = [
         choice2: "value",
         choice3: "var",
         choice4: "class",
-        answer:"var"
+        answer: "var"
     }
 ];
 
 // var timeCounter = 60;
 
-var reduceTime = function() {
+var reduceTime = function () {
     var timeCounter = 60;
 
-    var countdownInterval = setInterval(function() {
+    var countdownInterval = setInterval(function () {
         if (timeCounter > 0) {
-        timerEl.textContent = "Time Left: " + timeCounter;
-        timeCounter--;
+            timerEl.textContent = "Time Left: " + timeCounter;
+            timeCounter--;
         }
         else {
             clearInterval(countdownInterval);
-
         }
-    },1000);
+    }, 1000);
 }
 
-// startBtnEl.addEventListener("click", reduceTime());
+var askQuestions = function () {
+
+    for (var i = 0; i < questions.length; i++) {
+        questionEl.textContent = questions[i].question;
+        choice1El.textContent = questions[i].choice1;
+        choice2El.textContent = questions[i].choice2;
+        choice3El.textContent = questions[i].choice3;
+        choice4El.textContent = questions[i].choice4;
+        console.log(questionEl);
+
+        choiceContainerEl.addEventListener("click", responseHandler(event,choiceContainerEl));
+        
+        return questionContainerEl;
+    }
+
+};
+
+var responseHandler = function(event, currentQuestion) {
+    var currentChoice = event.target;
+    var responseEmoji = document.querySelector(".emoji");
+    if (currentQuestion.currentChoice === currentQuestion.answer) {
+        responseEmoji.textContent = "CORRECT! ✅"
+    }
+
+}
+
+var startQuiz = function () {
+    //  hide header
+    headerEl.setAttribute("style", "display:none;")
+    // hide Start Quiz button
+    startBtnEl.setAttribute("style", "display:none;")
+    // once the quiz begins, start the timer
+    timerEl.setAttribute("style", "display:'';")
+    reduceTime();
+
+    askQuestions();
+
+    // iterate through the questions by calling the askQuestions function
+
+
+};
+
+
+startBtnEl.addEventListener("click", startQuiz);
